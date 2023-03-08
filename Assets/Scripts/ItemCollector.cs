23421acf8cfa3446c53a3b28aa5cdd46a80c9fbb
im4aLL <me@habibhadi.com>
2023-03-08 01:57:00 -0500

    first commit

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int cherries = 0;
+    [SerializeField] private Text cherriesText;
+    [SerializeField] private AudioSource collectSoundEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            cherries++;
+            Destroy(collision.gameObject);
+            collectSoundEffect.Play();
+
+            cherriesText.text = "Cherries: " + cherries;
+        }   
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator animator;
+    private float dirX = 0f;
+    private SpriteRenderer sprite;
+    [SerializeField] private float jumpForce = 14f;
+    [SerializeField] private float moveSpeed = 7f;
+    private BoxCollider2D playerCollider;
+    [SerializeField] private LayerMask jumpableGround;
+    private enum MovementState { idle, running, jumping, falling }
+    private MovementState state = MovementState.idle;
+    [SerializeField] private AudioSource jumpSoundEffect;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        playerCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        
+        // when player jumps
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            jumpSoundEffect.Play();
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        UpdateAnimationState();
+    }
+
+    // Update animation state
+    // such as running
+    private void UpdateAnimationState()
+    {
+        // moving to right direction
+        if (dirX > 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        // moving to left direction
+        else if (dirX < 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+
+        // jumping
+        if (rb.velocity.y > 0.1f)
+        {
+            state = MovementState.jumping;
+        }
+        // falling
+        else if (rb.velocity.y < -0.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        animator.SetInteger("state", (int) state);
+    }
+
+    // check whether player is grounded or not
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
+    }
+}
